1978855e4003bed907b954c5d7916bb8e82bf899
Nicolas <jinqiucheng1006@live.cn>
2016-09-28 21:57:58 +0800

    Initial Commit

---
+++ FirebaseGuideDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirebaseGuideDemo
+//
+//  Created by 金秋成 on 16/9/28.
+//  Copyright © 2016年 金秋成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirebaseGuideDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirebaseGuideDemo
+//
+//  Created by 金秋成 on 16/9/28.
+//  Copyright © 2016年 金秋成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
